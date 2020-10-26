463e8f2c807f2ed76b47dd4f12af138df42eba2f
Lillian Paris <lillian.paris529@gmail.com>
2020-10-25 20:32:48 -0700

    Changed notes and index path

--- server.js
+++ server.js
@@ -31,11 +31,11 @@ app.use(express.static('public'));
 // HTML Routes
 
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
+  res.sendFile(path.join(__dirname, "/public/assets/notes.html"));
 });
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.sendFile(path.join(__dirname, "/public/assets/index.html"));
 });
 
 // Creating api route
