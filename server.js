7d953cd30bad1e5fd3a5428194bd87bc83ea8e1c
Lillian Paris <lillian.paris529@gmail.com>
2020-10-28 20:02:58 -0700

    Cleaning up server.js code and deleting comments

--- server.js
+++ server.js
@@ -1,25 +1,8 @@
-// * The following HTML routes should be created:
-
-//   * GET `/notes` - Should return the `notes.html` file.
-
-//   * GET `*` - Should return the `index.html` file
-
-// * The application should have a `db.json` file on the backend that will be used to store and retrieve notes using the `fs` module.
-
-// * The following API routes should be created:
-
-//   * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
-
-//   * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
-
-//   * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
-
 var express = require("express");
 var fs = require("fs");
 var path = require("path");
 
 // Sets up the Express App
-// =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -33,10 +16,6 @@ app.use(express.static('public'));
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
-
-// app.get("/", function (req, res) {
-//   res.sendFile(path.join(__dirname, "/public/assets/index.html"));
-// });
 
 // Creating api route
 app.get("/api/notes", function (req, res) {
