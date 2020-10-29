02779e720316ea4cce8e253c267ceb3405a3c4d2
Lillian Paris <lillian.paris529@gmail.com>
2020-10-28 19:13:33 -0700

    Fixed path to notes.html

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -31,19 +31,19 @@ app.use(express.static('public'));
 // HTML Routes
 
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "/public/assets/notes.html"));
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/public/assets/index.html"));
-});
+// app.get("/", function (req, res) {
+//   res.sendFile(path.join(__dirname, "/public/assets/index.html"));
+// });
 
 // Creating api route
 app.get("/api/notes", function (req, res) {
   fs.readFile("./db/db.json", function (err, data) {
     if (err) throw (err);
     let notes = JSON.parse(data)
-    return res.JSON(notes);
+    return res.json(notes);
   })
 });
 
